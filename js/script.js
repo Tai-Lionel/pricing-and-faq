dc948249d3a912455a12998fbdbaa77d370c09fd
Tai-Lionel <tailionel240687@gmail.com>
2024-02-23 14:07:00 +0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("navbar-scroll-effect");
+    } else {
+        navbar.classList.remove("navbar-scroll-effect");
+    }
+})
+
